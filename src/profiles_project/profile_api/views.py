d58d2e9defad99b511bc881277c873f53c4e7231
Elvis2131 <elartey2131@gmail.com>
2020-03-27 16:16:49 +0000

    Updating the viewsets with all http request methods

--- src/profiles_project/profile_api/views.py
+++ src/profiles_project/profile_api/views.py
@@ -44,6 +44,36 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """Testing API Viewset"""
+    serializer_class = HelloSerializer
 
     def list(self,request):
         return Response({'message':'List created successfully'})
+
+    def create(self,request):
+        """Post function for a viewset"""
+
+        serializer = HelloSerializer(data=request.POST)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            msg = "Hello {0}".format(name)
+
+            return Response({'Message':msg})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request,pk=None):
+        """ Handles getting an object via the pk"""
+        return Response({"Message":'Get an object'})
+
+    def update(self,request,pk=None):
+        """Handles updating an object"""
+        return Response({"Message":"Updating an object"})
+
+    def partial_update(self,request,pk=None):
+        """ Handles the patch http request"""
+        return Response({"Message":"Partial updating an object"})
+
+    def destroy(self,request,pk=None):
+        """ Handles the destroy function"""
+        return Response({"Message":"Deleting an object"})
+
