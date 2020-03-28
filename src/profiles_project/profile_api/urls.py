2022011d928482b823d1b48e3c573190f0a5a755
Elvis2131 <elartey2131@gmail.com>
2020-03-28 01:16:52 +0000

    Added Login Api

--- src/profiles_project/profile_api/urls.py
+++ src/profiles_project/profile_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 
 from rest_framework.routers import DefaultRouter
-from .views import HelloApiView, HelloViewSet, UserProfileViewset
+from .views import HelloApiView, HelloViewSet, UserProfileViewset,LoginViewSet
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, basename='helloviewset')
 router.register('profile', UserProfileViewset)
+router.register('login', LoginViewSet, basename='login')
 
 urlpatterns = [
     path('hello-view/', HelloApiView.as_view()),
--- src/profiles_project/profile_api/views.py
+++ src/profiles_project/profile_api/views.py
@@ -5,6 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from .models import *
 from .serializers import (HelloSerializer,UserProfileSerilaizer)
@@ -88,3 +91,14 @@ class UserProfileViewset(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns an auth token"""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self,request):
+        """Use the ObtainAuthToken APIView to validate and create a token"""
+        return ObtainAuthToken().post(request)
